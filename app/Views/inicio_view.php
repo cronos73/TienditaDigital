b4941e1ad95d297a929190c25fc22dbb0df53933
gopacjosechavez <jose.chavez@gopac.mx>
2023-01-13 13:34:18 -0600

    liberacion
    liberacion

--- app/Views/inicio_view.php
+++ app/Views/inicio_view.php
@@ -1,200 +1,104 @@
+<?php
+$idProducto = "1";
+$nombreProducto = "Nombre del producto";
+$descripcionProducto = "Descripción del producto para describir fd dfa dafa dafadadf afda daf dafa amas el producto ya que esto da a conocer un poco mas el producto.";
+$precioProducto = 11;
+$unidadVentaProducto = "litros";
+$existenciaProducto = "5";
+$cantidadProducto = "1";
+$familiaProducto = "Limpieza";
+
+?>
+
 <div class="row">
     <div id="card-1" class="card mb-3">
         <img src="<?php echo base_url() ?>/assets/image/tarjeta.jpg " class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Nombre del Producto</h5>
+            <h5 id="nombreProducto_<?php echo $idProducto ?>" class="card-title">
+                <?php echo $nombreProducto ?>
+            </h5>
             <small>
-                <p class="card-text" style="height:70px; overflow-x: hidden;overflow-y: auto;">
-                    Descripción del producto para describir fd dfa dafa dafadadf afda daf dafa amas el producto ya que esto da a conocer un poco mas el producto.
+                <p id="descripcionProducto_<?php echo $idProducto ?>" class="card-text" style="height:70px; overflow-x: hidden;overflow-y: auto;">
+                    <?php echo $descripcionProducto ?>
                 </p>
             </small>
             <hr>
+
             <table border="0" width="100%">
                 <tr>
-                    <td><small class="text-muted" width:"50%">Precio:</small></td>
-                    <td width:"50%">15.00</td>
+                    <td>
+                        <small class="text-muted" width:"50%">Precio:</small>
+                    </td>
+                    <td width:"50%">
+                        $
+                        <span id="precioProducto_<?php echo $idProducto ?>">
+                            <?php echo $precioProducto ?>
+                        </span>
+                        x
+                        <span class="unidadVentaProducto_<?php echo $idProducto ?>">
+                            <?php echo $unidadVentaProducto ?>
+                        </span>
+
+                    </td>
                 </tr>
                 <tr>
                     <td><small class="text-muted" width:"50%">Existencia:</small></td>
-                    <td width:"50%">5 litros</td>
+                    <td width:"50%">
+                        <span id="existenciaProducto_<?php echo $idProducto ?>">
+                            <?php echo $existenciaProducto ?>
+                        </span>
+                        <span class="unidadVentaProducto_<?php echo $idProducto ?>">
+                            <?php echo $unidadVentaProducto ?>
+                        </span>
+                    </td>
                 </tr>
                 <tr>
-                    <td><small class="text-muted" width:"50%">Cantidad:</small></td>
-                    <td width:"50%">1 litro</td>
+                    <td>
+                        <small class="text-muted" width:"50%">Cantidad:</small>
+                    </td>
+                    <td width:"50%">
+                        <button class="btn-cantidad" onclick="quitarCantidad(<?php echo $idProducto ?>)">
+                            <i class="fa fa-minus" aria-hidden="true"></i>
+                        </button>
+                        <b> 
+                        <span id="cantidadProducto_<?php echo $idProducto ?>">
+                           <?php echo $cantidadProducto ?> 
+                        </span>
+                        </b>
+                        <button class="btn-cantidad" onclick="agregarCantidad(<?php echo $idProducto ?>)">
+                            <i class="fa fa-plus" aria-hidden="true"></i>
+                        </button>
+                        <span class="unidadVentaProducto_<?php echo $idProducto ?>">
+                            <?php echo $unidadVentaProducto ?>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <small class="text-muted" width:"50%">Familia:</small>
+                    </td>
+                    <td width:"50%">
+                        <span id="familiaProducto_<?php echo $idProducto ?>">
+                            <?php echo $familiaProducto ?>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <h6 style="padding:40px;"><span id="observaciones_<?php echo $idProducto ?>" style="color:green;">Producto Disponible</span></h6>
+                    </td>
                 </tr>
             </table>
+
             <br>
             <p class="pull-right">
-                <a class="btn btn-primary btn-carrito" onclick="agregaCarrito()">
+                <a class="btn btn-primary btn-carrito" onclick="agregaCarrito(<?php echo $idProducto ?>)">
+                    <!-- <i class="fa fa-shopping-cart" aria-hidden="true" style="color:white"></i> -->
                     +
                 </a>
             </p>
         </div>
     </div>
 
-    <div id="card-2" class="card mb-3">
-        <img src="<?php echo base_url() ?>/assets/image/tarjeta.jpg " class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Nombre del Producto</h5>
-            <small>
-                <p class="card-text" style="height:70px; overflow-x: hidden;overflow-y: auto;">
-                    Descripción del producto para describir mas el producto ya que esto da a conocer un poco mas el producto.
-                </p>
-            </small>
-            <hr>
-            <table border="0" width="100%">
-                <tr>
-                    <td><small class="text-muted" width:"50%">Precio:</small></td>
-                    <td width:"50%">15.00</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Existencia:</small></td>
-                    <td width:"50%">5 litros</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Cantidad:</small></td>
-                    <td width:"50%">1 litro</td>
-                </tr>
-            </table>
-            <br>
-            <p class="pull-right">
-                <a class="btn btn-primary btn-carrito" onclick="agregaCarrito()">
-                    +
-                </a>
-            </p>
-
-        </div>
-    </div>
-    <div id="card-3" class="card mb-3">
-        <img src="<?php echo base_url() ?>/assets/image/tarjeta.jpg " class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Nombre del Producto</h5>
-            <small>
-                <p class="card-text" style="height:70px; overflow-x: hidden;overflow-y: auto;">
-                    Descripción del producto para describir fd dfa dafa dafadadf afda daf dafa amas el producto ya que esto da a conocer un poco mas el producto.
-                </p>
-            </small>
-            <hr>
-            <table border="0" width="100%">
-                <tr>
-                    <td><small class="text-muted" width:"50%">Precio:</small></td>
-                    <td width:"50%">15.00</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Existencia:</small></td>
-                    <td width:"50%">5 litros</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Cantidad:</small></td>
-                    <td width:"50%">1 litro</td>
-                </tr>
-            </table>
-            <br>
-            <p class="pull-right">
-                <a class="btn btn-primary btn-carrito" onclick="agregaCarrito()">
-                    +
-                </a>
-            </p>
-        </div>
-    </div>
-
-    <div id="card-4" class="card mb-3">
-        <img src="<?php echo base_url() ?>/assets/image/tarjeta.jpg " class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Nombre del Producto</h5>
-            <small>
-                <p class="card-text" style="height:70px; overflow-x: hidden;overflow-y: auto;">
-                    Descripción del producto para describir mas el producto ya que esto da a conocer un poco mas el producto.
-                </p>
-            </small>
-            <hr>
-            <table border="0" width="100%">
-                <tr>
-                    <td><small class="text-muted" width:"50%">Precio:</small></td>
-                    <td width:"50%">15.00</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Existencia:</small></td>
-                    <td width:"50%">5 litros</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Cantidad:</small></td>
-                    <td width:"50%">1 litro</td>
-                </tr>
-            </table>
-            <br>
-            <p class="pull-right">
-                <a class="btn btn-primary btn-carrito" onclick="agregaCarrito()">
-                    +
-                </a>
-            </p>
-        </div>
-    </div>
-
-    <div id="card-5" class="card mb-3">
-        <img src="<?php echo base_url() ?>/assets/image/tarjeta.jpg " class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Nombre del Producto</h5>
-            <small>
-                <p class="card-text" style="height:70px; overflow-x: hidden;overflow-y: auto;">
-                    Descripción del producto para describir fd dfa dafa dafadadf afda daf dafa amas .
-                </p>
-            </small>
-            <hr>
-            <table border="0" width="100%">
-                <tr>
-                    <td><small class="text-muted" width:"50%">Precio:</small></td>
-                    <td width:"50%">15.00</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Existencia:</small></td>
-                    <td width:"50%">5 litros</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Cantidad:</small></td>
-                    <td width:"50%">1 litro</td>
-                </tr>
-            </table>
-            <br>
-            <p class="pull-right">
-                <a class="btn btn-primary btn-carrito" onclick="agregaCarrito()">
-                    +
-                </a>
-            </p>
-        </div>
-    </div>
-
-    <div id="card-6" class="card mb-3">
-        <img src="<?php echo base_url() ?>/assets/image/tarjeta.jpg " class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Nombre del Producto</h5>
-            <small>
-                <p class="card-text" style="height:70px; overflow-x: hidden;overflow-y: auto;">
-                    Descripción del producto para describir mas el producto ya que esto da a conocer un poco mas el producto.
-                </p>
-            </small>
-            <hr>
-            <table border="0" width="100%">
-                <tr>
-                    <td><small class="text-muted" width:"50%">Precio:</small></td>
-                    <td width:"50%">15.00</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Existencia:</small></td>
-                    <td width:"50%">5 litros</td>
-                </tr>
-                <tr>
-                    <td><small class="text-muted" width:"50%">Cantidad:</small></td>
-                    <td width:"50%">1 litro</td>
-                </tr>
-            </table>
-            <br>
-            <p class="pull-right">
-                <a class="btn btn-primary btn-carrito" onclick="agregaCarrito()">
-                    +
-                </a>
-            </p>
-        </div>
-    </div>
 
 </div>
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -2,13 +2,44 @@
 
     <script src="<?php echo base_url() ?>/assets/bootstrap_5.3.0/js/bootstrap.bundle.min.js"></script>
     <script>
-      function agregaCarrito() {
-        var cantidad = parseInt(document.getElementById('carrito_cantidad').innerHTML);
-        if (cantidad > 0) {} else {
-          cantidad = 0;
+      function agregaCarrito(idProducto) {
+        var cantidad = parseInt(document.getElementById("cantidadProducto_" + idProducto).innerHTML);
+        var existencia = parseInt(document.getElementById("existenciaProducto_" + idProducto).innerHTML);
+        //document.getElementById("observaciones_" + idProducto).innerHTML = "";
+
+        if (cantidad <= existencia) {
+          existencia = existencia - cantidad;
+          document.getElementById("existenciaProducto_" + idProducto).innerHTML = existencia;
+
+          var cantidadCarrito = parseInt(document.getElementById('carrito_cantidad').innerHTML);
+          if (cantidadCarrito > 0) {
+
+          } else {
+            cantidadCarrito = 0;
+          }
+          cantidadCarrito = cantidadCarrito + cantidad;
+          document.getElementById('carrito_cantidad').innerHTML = cantidadCarrito;
+          document.getElementById("observaciones_" + idProducto).style.color="green";
+        }else{
+          document.getElementById("observaciones_" + idProducto).innerHTML = "Existencia agotada";
+          document.getElementById("observaciones_" + idProducto).style.color="red";
         }
-        cantidad++;
-        document.getElementById('carrito_cantidad').innerHTML = cantidad;
+        
+      }
+      function quitarCantidad(idProducto){
+        var cantidad = parseInt(document.getElementById("cantidadProducto_" + idProducto).innerHTML);
+        if (cantidad > 1){
+          cantidad--;
+        }
+        document.getElementById("cantidadProducto_" + idProducto).innerHTML = cantidad;
+      }
+      function agregarCantidad(idProducto){
+        var cantidad = parseInt(document.getElementById("cantidadProducto_" + idProducto).innerHTML);
+        var existencia = parseInt(document.getElementById("existenciaProducto_" + idProducto).innerHTML);
+        if (cantidad < existencia){
+          cantidad++;
+        }
+        document.getElementById("cantidadProducto_" + idProducto).innerHTML = cantidad;
       }
     </script>
     </body>
